f6f8439e0f794e44fa683032b7bd26380ecb28a5
Piotr <piotrektymula1@gmail.com>
2023-09-07 18:29:25 +0200

    setting up project

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding: 20px;
+`;
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>The Wild Oasis</H1>
+        <Button onClick={() => alert("Check in")}>Check in</Button>
+        <Button onClick={() => alert("Check out")}>Check out</Button>
+        <Input placeholder="Number of guests"></Input>
+      </StyledApp>
+    </>
+  );
+}
+
+export default App;
