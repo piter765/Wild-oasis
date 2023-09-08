f8dad823e628933313d09bbbac289d2509d3783f
Piotr <piotrektymula1@gmail.com>
2023-09-08 14:36:31 +0200

    spll layout, routes, created tables in supabase

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://xqcvqvgyigjfpmuohrme.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhxY3Zxdmd5aWdqZnBtdW9ocm1lIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQxNzA5MTgsImV4cCI6MjAwOTc0NjkxOH0.G8av57B1n-w-UPlB2mbrIBlNPh8n2NZyC87zSOkKicg';
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
